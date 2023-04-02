ca3a5803feb0a85e247acaeda4c389fdd71ab903
Iheb <iheb.dhouibi@polytechnicien.tn>
2023-04-02 16:57:49 +0100

    sql/ queries done

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
   // Define the SQL query to create the tables and the view, update certain attribute or select all rows
   const createEmployees = `
-     CREATE TABLE  employees (
-        employee_ID INT PRIMARY KEY,
+     CREATE TABLE  IF NOT EXISTS employees (
+        employee_id INT PRIMARY KEY,
         first_name VARCHAR(50) NOT NULL,
         middle_name VARCHAR(50),
         last_name VARCHAR(50) NOT NULL,
@@ -12,35 +12,37 @@
         education VARCHAR(50)
     );`
     const createSalaries = `
-    CREATE TABLE  salaries (
-        employee_ID INT PRIMARY KEY,
-        basic_salary DECIMAL(10,2) NOT NULL,
+    CREATE TABLE  IF NOT EXISTS salaries (
+        employee_id INT PRIMARY KEY,
+        basic_salary DECIMAL(10,3) NOT NULL,
         transport_allowance DECIMAL(10,2) NOT NULL,
         accommodation DECIMAL(10,2) ,
-        total_salary DECIMAL(10,2) NOT NULL,
-        FOREIGN KEY (employee_ID) REFERENCES employees(employee_ID)
+        total_salary DECIMAL(10,3) NOT NULL,
+        FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
       );`
       const createView = `
-      CREATE VIEW  employee_salaries AS
-        SELECT employees.employee_ID, first_name, last_name, basic_salary, transport_allowance, accommodation, total_salary
+      CREATE VIEW employee_salaries AS
+        SELECT employees.employee_ID, first_name, last_name,
+        nationality, date_of_birth, position, bank_account,
+         basic_salary, transport_allowance, accommodation, total_salary
         FROM employees
-        INNER JOIN salaries ON employees.employee_ID = salaries.employee_ID;
+        INNER JOIN salaries ON employees.employee_id = salaries.employee_id;
 
         `;
     const insertEmployee = " INSERT INTO employees ( "
-       + "employee_ID, first_name, middle_name, last_name, nationality, date_of_birth, position,bank_account, education)"
+       + "employee_id, first_name, middle_name, last_name, nationality, date_of_birth, position,bank_account, education)"
        + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
     const insertSalary = "INSERT INTO salaries ( "
-       + "employee_ID, basic_salary, transport_allowance, accommodation, total_salary  "
+       + "employee_id, basic_salary, transport_allowance, accommodation, total_salary) "
        + " values (?, ?, ?, ?, ?)";
     const updateEmployeeAttribute = `
         UPDATE employees
         SET {{attribute}} = ?
-        WHERE employee_ID = ?;`
+        WHERE employee_id = ?;`
     const updateSalariesAttribute = `
         UPDATE salaries
         SET {{attribute}} = ?
-        WHERE employee_ID = ?;    `
+        WHERE employee_id = ?;    `
     const retreiveEmployeeSalary = `SELECT * FROM employee_salaries;`
     module.exports = {createEmployees, createSalaries, createView, insertEmployee, insertSalary, updateEmployeeAttribute,
                         updateSalariesAttribute, retreiveEmployeeSalary}
